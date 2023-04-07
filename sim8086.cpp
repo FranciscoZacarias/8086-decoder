b377875886ff32257a20155da1a9492f1539a5c3
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2023-04-07 14:59:20 +0100

    MOV register/memory to/from register implemented

--- sim8086.cpp
+++ sim8086.cpp
@@ -2,12 +2,14 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#define LOW_8BITS(val) val & 0b0000000011111111
+
 enum Operation_Code {
 	INVALID = 0b00000000,
 	
 	// MOVs
-	MOV_REGMEM_TO_REGMEM = 0b100010,
-	MOV_IM_TO_REG        = 0b1011,
+	MOV_REGMEM_TOFROM_REG = 0b100010,
+	MOV_IM_TO_REG         = 0b1011,
 	
 };
 
@@ -21,17 +23,69 @@ struct Instruction {
 const char* byte_registers[8] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"}; // W = 0, 8  bit registers
 const char* word_registers[8] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"}; // W = 1, 16 bit registers
 
+// NOTE(fz): Second map contains displacement.
+const char* address_calc[8] = {"[bx + si", "[bx + di", "[bp + si", "[bp + di", "[si", "[di", "[bp", "[bx"};
+
+
 Operation_Code get_operation_code(uint8_t byte) {
-	// NOTE(fz): Op codes always start from the upper bits, so for example, 
-	if (((byte & 0b11110000) >> 4) == MOV_IM_TO_REG) {
-		return MOV_IM_TO_REG;
-	}
-	
-	if (((byte & 0b11111100) >> 2) == MOV_REGMEM_TO_REGMEM) {
-		return MOV_REGMEM_TO_REGMEM;
-	}
+	// NOTE(fz): Op codes always start from the upper bits
+	if (((byte & 0b11110000) >> 4) == MOV_IM_TO_REG)         return MOV_IM_TO_REG;
+	if (((byte & 0b11111100) >> 2) == MOV_REGMEM_TOFROM_REG) return MOV_REGMEM_TOFROM_REG;
 	
 	return INVALID;
+}
+
+// NOTE(fz):
+// D = 0, Instruction source      is specified in the REG field
+// D = 1, Instruction destination is specified in the REG field
+
+void write_mov_reg_to_reg(uint8_t reg_bits, uint8_t rm_bits, uint8_t D_bit, const char** register_table) {
+	if (D_bit) {
+		printf("mov %s, %s\n", register_table[reg_bits], register_table[rm_bits]);
+	} else {
+		printf("mov %s, %s\n", register_table[rm_bits], register_table[reg_bits]);
+	}
+}
+
+void write_mov_rm_tofrom_reg_no_displacement(uint8_t reg_bits, uint8_t rm_bits, uint8_t D_bit, const char** register_table) {
+	if (D_bit) {
+		printf("mov %s, %s]\n", register_table[reg_bits], address_calc[rm_bits]);
+	} else {
+		printf("mov %s], %s\n", address_calc[rm_bits], register_table[reg_bits]);
+	}
+}
+
+// NOTE(fz): Here I'm passing the W (wide) value instead of the register table directly, because I'd still need to know
+// the size of the registers to put in the format specifier of printf...
+void write_mov_rm_tofrom_reg_with_displacement(uint8_t reg_bits, uint8_t rm_bits, uint8_t D_bit, uint8_t W, uint8_t data_size, uint16_t data) {
+	// NOTE(fz): W only specifies if registers are 8 or 16 bits. It's independent from the type of data we receive.
+	const char** register_table = (W) ? word_registers : byte_registers;
+	
+	if (data_size) {
+		if (!data) {
+			if (D_bit) {
+				printf("mov %s, %s]\n", register_table[reg_bits], address_calc[rm_bits]);
+			} else {
+				printf("mov %s], %s\n", address_calc[rm_bits], register_table[reg_bits]);
+			}
+		} else if (D_bit) {
+			printf("mov %s, %s + %hu]\n", register_table[reg_bits], address_calc[rm_bits], data);
+		} else {
+			printf("mov %s + %hu], %s\n", address_calc[rm_bits], data, register_table[reg_bits]);
+		}
+	} else {
+		if (!data) {
+			if (D_bit) {
+				printf("mov %s, %s]\n", register_table[reg_bits], address_calc[rm_bits]);
+			} else {
+				printf("mov %s], %s\n", address_calc[rm_bits], register_table[reg_bits]);
+			}
+		} else if (D_bit) {
+			printf("mov %s, %s + %hhu]\n", register_table[reg_bits], address_calc[rm_bits], LOW_8BITS(data));
+		} else {
+			printf("mov %s + %hhu], %s\n", address_calc[rm_bits], LOW_8BITS(data), register_table[reg_bits]);
+		}
+	}
 }
 
 int main(int argc, char** argv) {	
@@ -46,7 +100,7 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-	printf("bits 16\n");
+	printf("bits 16\n\n");
 	
     struct Instruction instruction = { 0, 0 ,0 ,0 };
     while (fread(&instruction.byte1, sizeof(instruction.byte1), 1, fp) == 1) {
@@ -58,39 +112,65 @@ int main(int argc, char** argv) {
 		Operation_Code opcode = get_operation_code(instruction.byte1);
 		
 		switch(opcode) {
+			
 			case MOV_IM_TO_REG: {
 				fread(&instruction.byte2, sizeof(instruction.byte2), 1, fp);
 				
-				// W Byte
 				uint8_t W   = ((instruction.byte1 >> 3) & 1);
 				uint8_t reg = (instruction.byte1 & 0b00000111);
+				
 				if (W) {
 					fread(&instruction.byte3, sizeof(instruction.byte3), 1, fp);
-					printf("%s, %hu\n", word_registers[reg], ((instruction.byte3 << 8) | instruction.byte2));
+					printf("mov %s, %hu\n", word_registers[reg], ((instruction.byte3 << 8) | instruction.byte2));
 				} else {
-					printf("%s, %hhu\n", byte_registers[reg], instruction.byte2 );
+					printf("mov %s, %hhu\n", byte_registers[reg], instruction.byte2 );
 				}
 				
 			} break;
-			case MOV_REGMEM_TO_REGMEM: {
+			
+			case MOV_REGMEM_TOFROM_REG: {
 				fread(&instruction.byte2, sizeof(instruction.byte2), 1, fp);
 				
-				// W Byte
-				const char** register_table = ((instruction.byte1 & 0b00000001) & 0) ? byte_registers : word_registers;
-				uint8_t reg = ((instruction.byte2 & 0b00111000) >> 3);
+				uint8_t D = ((instruction.byte1 & 0b00000010) >> 1) & 1;
+				uint8_t W =  (instruction.byte1 & 0b00000001) & 1;
+				
+				uint8_t mod = (instruction.byte2 & 0b11000000) >> 6;
+				uint8_t reg = (instruction.byte2 & 0b00111000) >> 3;
 				uint8_t rm  = (instruction.byte2 & 0b00000111);
 				
-				// D Byte (direction)
-				uint8_t D = (((instruction.byte1 & 0b00000010) >> 1) & 0);
-				const char* source = (D) ? register_table[reg] : register_table[rm];
-				const char* dest   = (D) ? register_table[rm]  : register_table[reg];
-				
-				printf("mov %s, %s\n", dest, source);
+				switch(mod) {
+					
+					// Memory mode, no displacement
+					case 0b00: {
+						// TODO(fz): *Except when R/M = 110, then it's 16 bit displacement
+						const char** register_table = (W) ? word_registers : byte_registers;
+						write_mov_rm_tofrom_reg_no_displacement(reg, rm, D, register_table);
+					} break;
+					
+					// Memory mode, 8 bit displacement
+					case 0b01: {
+						fread(&instruction.byte3, sizeof(instruction.byte3),  1, fp);
+						write_mov_rm_tofrom_reg_with_displacement(reg, rm, D, W, 0, instruction.byte3);
+					} break;
+					
+					// Memory mode, 16 bit displacement
+					case 0b10: {
+						fread(&instruction.byte3, sizeof(instruction.byte3),  1, fp);
+						fread(&instruction.byte4, sizeof(instruction.byte4),  1, fp);
+						write_mov_rm_tofrom_reg_with_displacement(reg, rm, D, W, 1, ((instruction.byte4 << 8) | instruction.byte3));
+					} break;
+					
+					// Register Mode, no displacement
+					case 0b11: {
+						const char** register_table = (W) ? word_registers : byte_registers;
+						write_mov_reg_to_reg(reg, rm, D, register_table);
+					} break;
+				}
 			} break;
+			
 			case INVALID: {
 				printf("Invalid instruction\n");
 			}break;
-			
 		}
     }
 	
