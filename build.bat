041be8751c19e990e66a53fd0a6d2e511c19beb4
FranciscoZacarias <franciscobelchiorzacarias@gmail.com>
2023-06-04 11:10:15 +0100

    Setup for windows. Prepare listing 41

---
+++ build.bat
@@ -0,0 +1,8 @@
+@echo off
+
+if not defined VCVARS_RAN (
+    set "VCVARS_RAN=true"
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+)
+
+cl.exe /EHsc /I "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\include" sim8086.cpp
