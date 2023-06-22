09d0879c726c5efce220fa7f68a3554ce4cab21b
fzacarias <fzacarias@beyond-vision.pt>
2023-06-22 15:36:15 +0100

    ADD SUB and CMP now distinguish register sizes

--- Simulation.cpp
+++ Simulation.cpp
@@ -1,7 +1,25 @@
 
+void set_flags(s16 result, u8 offset) {
+	printf("  [Flags Set: ");
+
+	if (result == 0) {
+		printf("ZF");
+		// SET ZF
+		// UNSET CF SF
+	} else if (result > 0) {
+		// UNSET ZF CF SF
+	} else if (result < 0) {
+		printf("CF SF");
+		// CF SF
+		// UNSET ZF
+	}
+	printf("]");
+}
+
 void simulate_mov(SimRegister simRegisters[Register_count], InstructionOperand source, InstructionOperand destination, FILE* file) {
 	SimRegister* destinationSimRegister = &simRegisters[destination.register_access.reg];
 
+	fprintf(file, " %s: 0x%04hx (%d) -> ", get_reg_name(destination.register_access), destinationSimRegister->data16, destinationSimRegister->data16);
 	switch (source.type) {
 		case OperandType_Immediate: {
 			if (destination.register_access.offset == 0 && destination.register_access.count == 2) {
@@ -26,24 +44,28 @@ void simulate_add(SimRegister simRegisters[Register_count], InstructionOperand s
 	fprintf(file, " %s: 0x%04hx (%d) -> ", get_reg_name(destination.register_access), destinationSimRegister->data16, destinationSimRegister->data16);
 	switch (source.type) {
 		case OperandType_Immediate: {
-			if (destination.register_access.offset == 0 && destination.register_access.count == 2) {
+			if (source.register_access.count == 1) {
+				destinationSimRegister->data8[source.register_access.offset] += source.uImmediate;
+			} else if (source.register_access.count == 2) {
 				destinationSimRegister->data16 += source.uImmediate;
-
-			} else {
-				destinationSimRegister->data8[destination.register_access.offset] += source.uImmediate;
 			}
 		} break;
 
 		case OperandType_Register: {
 			SimRegister* sourceSimRegister = &simRegisters[source.register_access.reg];
 
-			// TODO(fz): For add, sub and cmp, we need to check if this is a 8bit or 16bit.
-			// Because, right now, this is just overriding the whole 16bit register, independent of the destination register size
-			destinationSimRegister->data16 += sourceSimRegister->data16;
+			// NOTE(fz): It's either 1 or 2 read count, I.e., 8 or 16bit register respectively
+			if (source.register_access.count == 1) {
+				destinationSimRegister->data8[source.register_access.offset] += sourceSimRegister->data8[source.register_access.offset];
+			} else if (source.register_access.count == 2) {
+				destinationSimRegister->data16 += sourceSimRegister->data16;
+			}
 		} break;
 	}
 
 	fprintf(file, "0x%04hx (%d)", destinationSimRegister->data16, destinationSimRegister->data16);
+
+	// NOTE(fz): If offset is 1, it gets the highest order bit of the first byte, if is 2, it gets the highest order bit of the second byte (the full 16bit register)
 }
 
 void simulate_sub(SimRegister simRegisters[Register_count], InstructionOperand source, InstructionOperand destination, FILE* file) {
@@ -52,47 +74,52 @@ void simulate_sub(SimRegister simRegisters[Register_count], InstructionOperand s
 	fprintf(file, " %s: 0x%04hx (%d) -> ", get_reg_name(destination.register_access), destinationSimRegister->data16, destinationSimRegister->data16);
 	switch (source.type) {
 		case OperandType_Immediate: {
-			if (destination.register_access.offset == 0 && destination.register_access.count == 2) {
+			if (source.register_access.count == 1) {
+				destinationSimRegister->data8[source.register_access.offset] -= source.uImmediate;
+			} else if (source.register_access.count == 2) {
 				destinationSimRegister->data16 -= source.uImmediate;
-			} else {
-				destinationSimRegister->data8[destination.register_access.offset] -= source.uImmediate;
 			}
 		} break;
 
 		case OperandType_Register: {
 			SimRegister* sourceSimRegister = &simRegisters[source.register_access.reg];
-			// TODO(fz): For add, sub and cmp, we need to check if this is a 8bit or 16bit.
-			// Because, right now, this is just overriding the whole 16bit register, independent of the destination register size
-			destinationSimRegister->data16 -= sourceSimRegister->data16;
+
+			// NOTE(fz): It's either 1 or 2 read count, I.e., 8 or 16bit register respectively
+			if (source.register_access.count == 1) {
+				destinationSimRegister->data8[source.register_access.offset] -= sourceSimRegister->data8[source.register_access.offset];
+			} else if (source.register_access.count == 2) {
+				destinationSimRegister->data16 -= sourceSimRegister->data16;
+			}
 		} break;
 	}
 
 	fprintf(file, "0x%04hx (%d)", destinationSimRegister->data16, destinationSimRegister->data16);
+
+	// NOTE(fz): If offset is 1, it gets the highest order bit of the first byte, if is 2, it gets the highest order bit of the second byte (the full 16bit register)
 }
 
 void simulate_cmp(SimRegister simRegisters[Register_count], InstructionOperand source, InstructionOperand destination, FILE* file) {
 	SimRegister* destinationSimRegister = &simRegisters[destination.register_access.reg];
+	s32 result = 0;
 
-
-	fprintf(file, " %s: 0x%04hx (%d) -> ", get_reg_name(destination.register_access), destinationSimRegister->data16, destinationSimRegister->data16);
+	fprintf(file, " %s: ", get_reg_name(destination.register_access));
 	switch (source.type) {
 		case OperandType_Immediate: {
-			if (destination.register_access.offset == 0 && destination.register_access.count == 2) {
-				destinationSimRegister->data16 -= source.uImmediate;
-			} else {
-				destinationSimRegister->data8[destination.register_access.offset] -= source.uImmediate;
-			}
+			result = (source.register_access.count == 1) ?
+				(destinationSimRegister->data8[source.register_access.offset] - source.uImmediate) : (destinationSimRegister->data16 - source.uImmediate);
+
 		} break;
 
 		case OperandType_Register: {
 			SimRegister* sourceSimRegister = &simRegisters[source.register_access.reg];
-			// TODO(fz): For add, sub and cmp, we need to check if this is a 8bit or 16bit.
-			// Because, right now, this is just overriding the whole 16bit register, independent of the destination register size
-			destinationSimRegister->data16 -= sourceSimRegister->data16;
+
+			// NOTE(fz): It's either 1 or 2 read count, I.e., 8 or 16bit register respectively
+			result = (source.register_access.count == 1) ?
+				(destinationSimRegister->data8[source.register_access.offset] - sourceSimRegister->data8[source.register_access.offset]) : (destinationSimRegister->data16 - sourceSimRegister->data16);
+
 		} break;
 	}
 
-	fprintf(file, "0x%04hx (%d)", destinationSimRegister->data16, destinationSimRegister->data16);
 }
 
 void simulate_instruction(SimRegister simRegisters[Register_count], Instruction instruction, FILE* file) {
